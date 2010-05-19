169a264cf32945587f103821f9547254d0f30b1e
nathan <nathan@nc10.(none)>
2010-05-19 23:47:50 +0100

    Damn paths

--- index.py
+++ index.py
@@ -30,7 +30,7 @@ class Data:
     pass
 # G is the global data struct used everywhere in webr.py!
 G = Data()
-G.user, G.sets_l, G.sets_d, G.photos_d = cPickle.load(open(os.path.join(abspath,config.FlickrDBFile)))
+G.User, G.sets_l, G.sets_d, G.photos_d = cPickle.load(open(os.path.join(abspath,config.FlickrDBFile)))
 G.root = config.SiteRoot
 G.media = config.MediaRoot
 G.name = config.GalleryName
