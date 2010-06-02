10eb91059bc48824ae0e143065f8e1b919dcb3a9
nh <nhackett1@gmail.com>
2010-06-02 15:31:49 +0100

    First commit from machine pris

--- update.py
+++ update.py
@@ -1,7 +1,7 @@
 import flickrapi
 import web
 
-Fuser = "25721820@N00"
+Fuser = "47608356@N06"
 db = "gal.db"
 
 Fkey = "04133797314d6ff600f07314fe061643"
@@ -11,15 +11,16 @@ f = flickrapi.FlickrAPI(Fkey, Fsecret)
 
 db = web.database(dbn='sqlite',db='gal.db')
 
-photos = f.people_GetPublicPhotos(api_key = Fkey, user_id = Fuser, per_page=500)
+photos = f.people_GetPublicPhotos(api_key = Fkey, user_id = Fuser, per_page=500, extras="date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_sq, url_t, url_s, url_m, url_o")
 
 for p in photos[0]:
-    print p.attrib['id']
+    for a in p.attrib:
+        print a + " -- " + p.attrib[a]
+    print "------------------------"
 print photos[0].attrib['total']
 
 L1 = (0,1,2,3,4,5,6,7)
 L2 = (0,1,2,3,44,5,6,7)
 
-L2 = dict([(k,None) for k in L2])
 print [item for item in L1 if item not in L2]
 print [item for item in L2 if item not in L1]
