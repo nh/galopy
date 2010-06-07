6d31f042ba527a48c6f9d3f76fc684ff3f4ccc58
nh <nhackett1@gmail.com>
2010-06-07 14:20:33 +0100

    Added wsgi

--- index.py
+++ index.py
@@ -1,140 +1,94 @@
-#
-# webr.py: A virtual photo gallery software. It lets you showcase your flickr photosets 
-#          in a photo gallery on your domain, styled to your tastes.
-#      
-# author:   Deepak Sarda
-#           firstname@antrix.net
-# requires: web.py (http://webpy.org)
-#           Cheetah template (http://www.cheetahtemplate.org/)
-#           flup (for web.py)
-# Changelog: http://antrix.net/stuff/webr/Changelog
+import flickrapi
+import web
 
-__author__      = 'Deepak Sarda'
-__version__     = '0.3'
-__copyright__   = '(c) 2006 Deepak Sarda'
-__license__     = 'GPL'
-__url__         = 'http://www.antrix.net/stuff/webr/'
+Fuser = "54827292@N00" #me
+Fuser = "25721820@N00" #joz
 
-import sys, os
-abspath = os.path.dirname(__file__)
-sys.path.append(abspath)
+db = "gal.db"
 
-import cPickle, web, config, webr_update
-from webr_update import User, Photo, PhotoSet
+Fkey = "04133797314d6ff600f07314fe061643"
+Fsecret = "b9f548992d4c62b0"
 
-web.config.debug = True
+f = flickrapi.FlickrAPI(Fkey, Fsecret)
 
+db = web.database(dbn='sqlite',db='gal.db')
 render = web.template.render('templates/')
 
-class Data:
-    pass
-# G is the global data struct used everywhere in webr.py!
-G = Data()
-G.user, G.sets_l, G.sets_d, G.photos_d = cPickle.load(open(os.path.join(abspath,config.FlickrDBFile)))
-G.root = config.SiteRoot
-G.media = config.MediaRoot
-G.name = config.GalleryName
-
-#### Helper functions ####
-import mimetypes
-def mime_type(filename):
-    return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
-
-def getSets(p_id):
-    """Return a list of set ids in which photo with id: p_id, exists"""
-    p_id = str(p_id) # To be sure!
-    sets = [s for s in G.sets_l if p_id in G.sets_d[s].photos]
-    return sets 
-#end getSets
-
-def getSiblings(p_id, s_id):
-    """Return next and previous photo ids for p_id in set 's_id'
-    Careful! Doesn't check if p_id exists in s_id.
-    """
-    # This function will fail if set contains just one photo ;-)
-    s = G.sets_d[str(s_id)]
-    i = s.photos.index(str(p_id))
-    
-    if i == 0:
-        return None, s.photos[1]
-    elif i == len(s.photos) - 1:
-        return s.photos[-2], None
-    else:
-        return s.photos[i-1], s.photos[i+1]
-#end getSiblings
-
-#### CONTROLLERS ####
-
-class IndexC:
+class showSetlist:
     def GET(self):
-        # We want the sets in the same order they are sorted on Flickr
-        sets = [ G.sets_d[s] for s in G.sets_l ]
-        return render.index(sets,G)
-#end IndexC
+        return render.sets(list(db.select("sets")))
 
-class SetC:
-    def GET(self, set_id):
-        if str(set_id) not in G.sets_d:
-            return web.notfound()
-        
-        photoset = G.sets_d[str(set_id)]
-        primary = G.photos_d[photoset.primary]
-        photos = [G.photos_d[p] for p in photoset.photos]
-        return render.set(photoset,primary,photos,G)
-#end SetC
-    
-class PhotoC:
-    def GET(self, p_id, set_id = ''):
-        # Wish web.py's urlmapper did this for me!
-        p_id = str(p_id)
-        set_id = str(set_id)
-        
-        if p_id not in G.photos_d:
-            return web.notfound()
-        
-        if not set_id:
-            this_set = None
-        elif set_id not in G.sets_d:
-            return web.redirect('/%s/' % p_id)
-        else: 
-            this_set = G.sets_d[set_id]
-            
-        if this_set:
-            prev, next = getSiblings(p_id, this_set.id)
-            if prev: prev = G.photos_d[prev]
-            if next: next = G.photos_d[next]
-        else:
-            prev, next = None, None
+class updateSetlist:
+    def GET(self):
+        sets = f.photosets_GetList(api_key = Fkey, user_id = Fuser)[0]
+        dbsets = [str(x["id"]) for x in db.select("sets",what="id")]
+        for newset in [item for item in sets if item.attrib["id"] not in dbsets]:
+            db.insert("sets",id=newset.attrib["id"],pcount=newset.attrib["photos"],pri=newset.attrib["primary"],thumb="blank.gif",title=newset[0].text,desc=newset[1].text)
+            print 'Added set %s' % (newset[0].text)
+        for deadset in [item for item in dbsets if item not in [y.attrib["id"] for y in sets]]:
+            db.delete("sets",where="id="+deadset)
+            print 'Deleted set %s' % (deadset)
 
-        photo = G.photos_d[p_id]
-        if not photo.title:
-            photo.title = 'Untitled'
-        
-        other_sets = getSets(p_id)
-        other_sets = [ G.sets_d[s] for s in other_sets if s != set_id]
-        
-        return render.photo(photo,this_set,other_sets,prev,next,G)
-#end PhotoC
+        setlist = list(db.select("sets"))
+        return render.sets(setlist)
 
-class StaticServerC:
-    def GET(self, static_dir):
-        try:
-            static_file_name = web.ctx.path.split('/')[-1]
-            web.header('Content-type', mime_type(static_file_name))
-            static_file = open('.' + web.ctx.path, 'rb').read()
-            web.ctx.output = static_file
-        except IOError:
-            web.notfound()
+def setUpdater(setid):
+    imgs = f.photosets_GetPhotos(api_key = Fkey, photoset_id = setid,extras="last_update,url_sq,url_m")[0]
+    dbimgsids = [str(x.id) for x in db.query("SELECT imgs.id FROM imgs INNER JOIN keys ON imgs.id = keys.img_id AND keys.set_id = "+setid)]
 
-#### END CONTROLLERS #### 
-        
+    #for newimg in [item for item in imgs if item.attrib["id"] not in dbimgsids]:
+    #    db.insert("keys",img_id=newimg.attrib["id"],set_id=setid)
+    #    db.insert("imgs",id=newimg.attrib["id"],title=newimg.attrib["title"],lastupdate=newimg.attrib["lastupdate"],url_sq=newimg.attrib["url_sq"],url_m=newimg.attrib["url_m"])
+
+    for newimg in imgs:
+        #for newimg in [item for item in imgs if item.attrib["id"] not in dbimgsids]:
+        db.query("INSERT OR IGNORE INTO keys VALUES ("+newimg.attrib["id"]+","+setid+")")
+        #db.query("INSERT OR IGNORE INTO imgs (id,title,lastupdate,url_sq,url_m) VALUES ($id,$title,$lastupdate,$url_sq,$url_m)", vars=newimg.attrib)
+
+    for changedimg in [x for x in imgs if int(x.attrib["lastupdate"]) > db.select("imgs",what="lastupdate",where="id="+x.attrib["id"])[0].lastupdate]:
+        db.update("imgs",where="id="+changedimg.attrib["id"],title=changedimg.attrib["title"],lastupdate=changedimg.attrib["lastupdate"])
+        print "update %s" % changedimg.attrib["title"]
+
+    for deadimg in [item for item in dbimgsids if item not in [y.attrib["id"] for y in imgs]]:
+        db.delete("keys",where="img_id="+deadimg)
+        db.delete("imgs",where="id="+deadimg)
+        print 'Deleted image %s' % (deadimg)
+
+class showSet:
+    def GET(self,id):
+        set = db.select("sets",where="id = "+id)[0]
+        imglist = list(db.query("SELECT * FROM imgs INNER JOIN keys ON imgs.id = keys.img_id AND keys.set_id = "+id))
+        return render.setimgs(set,imglist)
+
+class updateSet:
+    def GET(self,id):
+        setUpdater(id)
+        set = db.select("sets",where="id = "+id)[0]
+        imglist = list(db.query("SELECT * FROM imgs INNER JOIN keys ON imgs.id = keys.img_id AND keys.set_id = "+id))
+        return render.setimgs(set,imglist)
+
+"""
+photos = f.people_GetPublicPhotos(api_key = Fkey, user_id = Fuser, per_page=500, extras="date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_sq, url_t, url_s, url_m, url_o")
+for p in photos[0]:
+    for a in p.attrib:
+        print a + " -- " + p.attrib[a]
+    print "------------------------"
+print photos[0].attrib['total']
+"""
+
 urls = (
-    '/(templates)/.*', 'StaticServerC',
-    '/set/(\d+)/?', 'SetC',
-    '/(\d+)/in/set-(\d+)/?', 'PhotoC',
-    '/(\d+)/?', 'PhotoC',
-    '/?', 'IndexC',
+    "/sets/update","updateSetlist",
+    "/sets", "showSetlist",
+    "/sets/update/(.*)","updateSet",
+    "/sets/(.*)","showSet",
 )
 
-app = web.application(urls, globals(), autoreload=False)
-application = app.wsgifunc()
+L1 = (0,1,2,3,4,5,6,7)
+L2 = (0,1,2,3,44,5,6,7)
+
+print [item for item in L1 if item not in L2]
+print [item for item in L2 if item not in L1]
+
+app = web.application(urls, globals())
+web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
+if __name__ == "__main__": app.run()
