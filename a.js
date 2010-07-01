ad7afdaec60726853bf304befff4bdeef3eaebb3
Nathan <n@pris.(none)>
2010-07-01 09:42:25 +0100

    Functional admin

---
+++ a.js
@@ -0,0 +1,11 @@
+$().ready(function() {
+    $('ul li a:nth-child(2)').mousedown(function() {
+        var link = $(this);
+        var progress = setInterval(function() { link.load("/status"); }, 500);
+        $.get(link.attr("href"),function(){
+            clearInterval(progress);
+            link.text("Done");
+        });
+        return false;
+    }).click(function(){return false;});
+});
--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import flickrapi
 import web
-
+import time
+import elementtree.ElementTree as ET
 Fuser = "54827292@N00" #me
 Fuser = "25721820@N00" #joz
 
@@ -13,17 +14,38 @@ f = flickrapi.FlickrAPI(Fkey, Fsecret)
 
 db = web.database(dbn='sqlite',db='gal.db')
 render = web.template.render('templates/')
+progress = [0,100]
+
+class UpdateStatus:
+    def __init__(self):
+        self.progressbar = 0
+
 
 class showSetlist:
     def GET(self):
         return render.sets(list(db.select("sets")))
 
-class updateSetlist:
+class dropsets:
+    def GET(self):
+        db.query("DELETE FROM imgs")
+        db.query("DELETE FROM sets")
+        db.query("DELETE FROM keys")
+        return "dropped sets!"
+
+class UpdateSetlist:
     def GET(self):
         sets = f.photosets_GetList(api_key = Fkey, user_id = Fuser)[0]
         dbsets = [str(x["id"]) for x in db.select("sets",what="id")]
         for newset in [item for item in sets if item.attrib["id"] not in dbsets]:
-            db.insert("sets",id=newset.attrib["id"],pcount=newset.attrib["photos"],pri=newset.attrib["primary"],thumb="blank.gif",title=newset[0].text,desc=newset[1].text)
+            db.insert(
+                "sets",
+                id=newset.attrib["id"],
+                pcount=newset.attrib["photos"],
+                pri=newset.attrib["primary"],
+                thumb="http://farm%s.static.flickr.com/%s/%s_%s_s.jpg" % (newset.attrib["farm"],newset.attrib["server"],newset.attrib["primary"],newset.attrib["secret"]),  
+                title=newset[0].text,
+                desc=newset[1].text
+            )
             print 'Added set %s' % (newset[0].text)
         for deadset in [item for item in dbsets if item not in [y.attrib["id"] for y in sets]]:
             db.delete("sets",where="id="+deadset)
@@ -32,27 +54,33 @@ class updateSetlist:
         setlist = list(db.select("sets"))
         return render.sets(setlist)
 
-def setUpdater(setid):
-    imgs = f.photosets_GetPhotos(api_key = Fkey, photoset_id = setid,extras="last_update,url_sq,url_m")[0]
-    dbimgsids = [str(x.id) for x in db.query("SELECT imgs.id FROM imgs INNER JOIN keys ON imgs.id = keys.img_id AND keys.set_id = "+setid)]
+class UpdateImgs:
+    def __init__(self,setids):
+        global progress
+        imgs = ET.Element("imgs")
+        for setid in setids:
+            imgs.insert(0,f.photosets_GetPhotos(api_key = Fkey, photoset_id = setid,extras="last_update,url_sq,url_m")[0])
+        imgs = imgs[0]
+        progress = [0,len(imgs)]
+        setsquery = "".join(["keys.set_id = %s OR" % k for k in setids]).rstrip(" OR")
+        dbimgsids = [str(x.id) for x in db.query("SELECT imgs.id FROM imgs INNER JOIN keys ON imgs.id = keys.img_id AND (" + setsquery + ")")]
+        print dbimgsids
+        for newimg in [item for item in imgs if item.attrib["id"] not in dbimgsids]: # All new images that are not already in this set
+            #db.query("INSERT OR IGNORE INTO keys VALUES ("+newimg.attrib["id"]+","+setid+")")
+            db.query("INSERT OR IGNORE INTO imgs (id,title,lastupdate,url_sq,url_m) VALUES ($id,$title,$lastupdate,$url_sq,$url_m)", vars=newimg.attrib) # Don't add image to imglist if it already exists in another set...
+            db.insert("keys",img_id=newimg.attrib["id"],set_id=setid) # ...but still add to this set's imglist
+            #time.sleep(3)
+            progress[0] += 1   
 
-    #for newimg in [item for item in imgs if item.attrib["id"] not in dbimgsids]:
-    #    db.insert("keys",img_id=newimg.attrib["id"],set_id=setid)
-    #    db.insert("imgs",id=newimg.attrib["id"],title=newimg.attrib["title"],lastupdate=newimg.attrib["lastupdate"],url_sq=newimg.attrib["url_sq"],url_m=newimg.attrib["url_m"])
+        for changedimg in [x for x in imgs if int(x.attrib["lastupdate"]) > db.select("imgs",what="lastupdate",where="id="+x.attrib["id"])[0].lastupdate]:
+            db.update("imgs",where="id="+changedimg.attrib["id"],title=changedimg.attrib["title"],lastupdate=changedimg.attrib["lastupdate"])
+            print "update %s" % changedimg.attrib["title"]
 
-    for newimg in imgs:
-        #for newimg in [item for item in imgs if item.attrib["id"] not in dbimgsids]:
-        db.query("INSERT OR IGNORE INTO keys VALUES ("+newimg.attrib["id"]+","+setid+")")
-        #db.query("INSERT OR IGNORE INTO imgs (id,title,lastupdate,url_sq,url_m) VALUES ($id,$title,$lastupdate,$url_sq,$url_m)", vars=newimg.attrib)
-
-    for changedimg in [x for x in imgs if int(x.attrib["lastupdate"]) > db.select("imgs",what="lastupdate",where="id="+x.attrib["id"])[0].lastupdate]:
-        db.update("imgs",where="id="+changedimg.attrib["id"],title=changedimg.attrib["title"],lastupdate=changedimg.attrib["lastupdate"])
-        print "update %s" % changedimg.attrib["title"]
-
-    for deadimg in [item for item in dbimgsids if item not in [y.attrib["id"] for y in imgs]]:
-        db.delete("keys",where="img_id="+deadimg)
-        db.delete("imgs",where="id="+deadimg)
-        print 'Deleted image %s' % (deadimg)
+        for deadimg in [item for item in dbimgsids if item not in [y.attrib["id"] for y in imgs]]:
+            db.delete("keys",where="img_id="+deadimg)
+            db.delete("imgs",where="id="+deadimg)
+            print 'Deleted image %s' % (deadimg)
+        progress = [0,100]
 
 class showSet:
     def GET(self,id):
@@ -62,10 +90,29 @@ class showSet:
 
 class updateSet:
     def GET(self,id):
-        setUpdater(id)
-        set = db.select("sets",where="id = "+id)[0]
-        imglist = list(db.query("SELECT * FROM imgs INNER JOIN keys ON imgs.id = keys.img_id AND keys.set_id = "+id))
-        return render.setimgs(set,imglist)
+        UpdateImgs([id])
+        #set = db.select("sets",where="id = "+id)[0]
+        #imglist = list(db.query("SELECT * FROM imgs INNER JOIN keys ON imgs.id = keys.img_id AND keys.set_id = "+id))
+        raise web.seeother('/sets/'+id)
+
+class UpdateStatus:
+    def GET(self):
+        global progress
+        status = int((1.0*progress[0]/progress[1])*100)
+        if status == 0:
+            return "Done"
+        else:
+            return str(status)+"%"
+
+class admincss:
+    def GET(self):
+        web.header('Content-Type', 'text/css')
+        return open('a.css','r').read()
+
+class adminjs:
+    def GET(self):
+        web.header('Content-Type', 'text/javascript')
+        return open('jquery.js','r').read() + open('a.js','r').read()
 
 """
 photos = f.people_GetPublicPhotos(api_key = Fkey, user_id = Fuser, per_page=500, extras="date_upload, date_taken, owner_name, icon_server, original_format, last_update, geo, tags, machine_tags, o_dims, views, media, path_alias, url_sq, url_t, url_s, url_m, url_o")
@@ -77,13 +124,17 @@ print photos[0].attrib['total']
 """
 
 urls = (
-    "/sets/update","updateSetlist",
+    "/a.js","adminjs",
+    "/a.css","admincss",
+    "/status","UpdateStatus",
+    "/dropsets","dropsets",
+    "/sets/update","UpdateSetlist",
     "/sets", "showSetlist",
     "/sets/update/(.*)","updateSet",
     "/sets/(.*)","showSet",
 )
 
-#app = web.application(urls, globals())
-application = web.application(urls, globals()).wsgifunc()
+app = web.application(urls, globals())
+#application = web.application(urls, globals()).wsgifunc()
 if __name__ == "__main__":
     app.run()
